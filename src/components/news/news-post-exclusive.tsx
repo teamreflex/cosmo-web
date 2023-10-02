c7104d2c48f0012956a57055f25679f8c41a4ae0
xKairu <kairu@team-reflex.com>
2023-10-02 23:53:13 +1300

    add video playback for exclusive content

--- src/components/news/news-post-exclusive.tsx
+++ src/components/news/news-post-exclusive.tsx
@@ -1,28 +1,25 @@
+"use client";
+
 import { CosmoNewsSectionExclusiveContent } from "@/lib/server/cosmo";
 import Image from "next/image";
 import Link from "next/link";
 import Timestamp from "../ui/timestamp";
+import ReactPlayer from "react-player";
 
 export default function NewsPostExclusive({
   post,
 }: {
   post: CosmoNewsSectionExclusiveContent;
 }) {
+  const isVideoPost = post.url.includes("update-notice");
+
   return (
     <div className="flex flex-col gap-2 w-full">
-      <Link
-        href={post.url}
-        target="_blank"
-        className="relative aspect-video rounded-xl border border-accent overflow-hidden"
-      >
-        <Image
-          src={post.thumbnailImageUrl}
-          alt={post.title}
-          fill={true}
-          className="object-contain"
-          quality={100}
-        />
-      </Link>
+      {isVideoPost ? (
+        <ExclusiveVideoPost post={post} />
+      ) : (
+        <ExclusiveLinkPost post={post} />
+      )}
       <div className="flex flex-col">
         <p className="font-bold">{post.title}</p>
         <p className="text-sm">{post.body}</p>
@@ -30,6 +27,46 @@ export default function NewsPostExclusive({
           <Timestamp timestamp={post.createdAt} />
         </p>
       </div>
+    </div>
+  );
+}
+
+function ExclusiveLinkPost({
+  post,
+}: {
+  post: CosmoNewsSectionExclusiveContent;
+}) {
+  return (
+    <Link
+      href={post.url}
+      target="_blank"
+      className="relative aspect-video rounded-xl border border-accent overflow-hidden"
+    >
+      <Image
+        src={post.thumbnailImageUrl}
+        alt={post.title}
+        fill={true}
+        className="object-contain"
+        quality={100}
+      />
+    </Link>
+  );
+}
+
+function ExclusiveVideoPost({
+  post,
+}: {
+  post: CosmoNewsSectionExclusiveContent;
+}) {
+  return (
+    <div className="cursor-pointer relative aspect-video rounded-xl border border-accent overflow-hidden">
+      <ReactPlayer
+        url={post.nativeVideoUrl}
+        controls={true}
+        width="100%"
+        height="100%"
+        light={post.thumbnailImageUrl}
+      />
     </div>
   );
 }
