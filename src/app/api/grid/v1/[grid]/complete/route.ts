ca26afb670e6aca84c733f5ac9fc29517c1e66c1
xKairu <kairu@team-reflex.com>
2023-10-09 02:36:25 +1300

    fix grid api routes

--- src/app/api/grid/v1/[grid]/complete/route.ts
+++ src/app/api/grid/v1/[grid]/complete/route.ts
@@ -1,16 +1,22 @@
 import { getUser } from "@/app/api/common";
+import { env } from "@/env.mjs";
 import { CosmoGridSlotCompletion, completeGrid } from "@/lib/server/cosmo";
 import { NextRequest, NextResponse } from "next/server";
 
 export const runtime = "edge";
 
-export async function GET(
+export async function POST(
   request: NextRequest,
   { params }: { params: { grid: string } }
 ) {
   const auth = await getUser();
   if (!auth.success) {
     return new Response(auth.error, { status: auth.status });
+  }
+
+  // dev: simulate grid completion
+  if (env.NEXT_PUBLIC_SIMULATE_GRID) {
+    return NextResponse.json({ success: true }, { status: 200 });
   }
 
   const { slots }: { slots: CosmoGridSlotCompletion[] } = await request.json();
