c176246ddfd857cf24def7af199a1cc1cafd4fd6
xKairu <kairu@team-reflex.com>
2023-10-30 10:20:00 +1300

    fix scroll issue

--- src/app/(core)/layout.tsx
+++ src/app/(core)/layout.tsx
@@ -24,28 +24,30 @@ export default async function CoreLayout({
 
   return (
     <ClientProviders>
-      <Navbar
-        user={user}
-        artists={artists}
-        selectedArtist={selectedArtist as ValidArtist | undefined}
-        comoBalances={
-          user ? (
-            <Suspense
-              fallback={
-                <div className="flex items-center">
-                  <Loader2 className="animate-spin" />
-                </div>
-              }
-            >
-              <ComoBalances address={user.address} artists={artists} />
-            </Suspense>
-          ) : null
-        }
-      />
+      <div className="relative flex min-h-screen flex-col">
+        <Navbar
+          user={user}
+          artists={artists}
+          selectedArtist={selectedArtist as ValidArtist | undefined}
+          comoBalances={
+            user ? (
+              <Suspense
+                fallback={
+                  <div className="flex items-center">
+                    <Loader2 className="animate-spin" />
+                  </div>
+                }
+              >
+                <ComoBalances address={user.address} artists={artists} />
+              </Suspense>
+            ) : null
+          }
+        />
 
-      {/* content */}
-      <div className="flex min-h-screen min-w-full flex-col text-foreground">
-        {children}
+        {/* content */}
+        <div className="flex min-w-full flex-col text-foreground">
+          {children}
+        </div>
       </div>
     </ClientProviders>
   );
