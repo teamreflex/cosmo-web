668e539afb0b422308f1e68fb471cef0be411c7d
xKairu <kairu@team-reflex.com>
2023-11-19 00:25:28 +1300

    update drizzle-kit config

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,15 +1,11 @@
-import type { Config } from "drizzle-kit";
-import * as dotenv from "dotenv";
-dotenv.config({
-  path: "./.env.local",
-});
+import { defineConfig } from "drizzle-kit";
 
 const uri = `mysql://${process.env.DATABASE_USERNAME}:${process.env.DATABASE_PASSWORD}@${process.env.DATABASE_HOST}/${process.env.DATABASE_NAME}?ssl={"rejectUnauthorized":true}`;
 
-export default {
+export default defineConfig({
   schema: "./src/lib/server/db/schema.ts",
   driver: "mysql2",
   dbCredentials: {
     uri,
   },
-} satisfies Config;
+});
