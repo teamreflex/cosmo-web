8fe4783346df246d8bb6408d0af3e911d6d54a59
xKairu <kairu@team-reflex.com>
2023-11-18 18:00:46 +1300

    switch to node-postgres

--- src/lib/server/db/indexer/index.ts
+++ src/lib/server/db/indexer/index.ts
@@ -1,14 +1,15 @@
-import { drizzle } from "drizzle-orm/postgres-js";
-import postgres from "postgres";
+import { drizzle } from "drizzle-orm/node-postgres";
+import { Client } from "pg";
 import * as schema from "./schema";
 import { env } from "@/env.mjs";
 
-const client = postgres({
+const client = new Client({
   host: env.INDEXER_DB_HOST,
-  username: env.INDEXER_DB_USERNAME,
+  user: env.INDEXER_DB_USERNAME,
   password: env.INDEXER_DB_PASSWORD,
   port: env.INDEXER_DB_PORT,
   database: env.INDEXER_DB_NAME,
 });
 
+await client.connect();
 export const indexer = drizzle(client, { schema });
