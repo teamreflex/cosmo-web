239bb23513a9c1d8f46bd3c6bdb9c108ea329275
xKairu <kairu@team-reflex.com>
2023-12-01 07:18:49 +1300

    uninstall sentry

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,3 @@
-import { withSentryConfig } from "@sentry/nextjs";
 import "./src/env.mjs";
 
 /** @type {import('next').NextConfig} */
@@ -49,34 +48,4 @@ const config = {
   },
 };
 
-export default withSentryConfig(
-  config,
-  {
-    // Suppresses source map uploading logs during build
-    silent: true,
-    org: "team-reflex",
-    project: "apollo",
-  },
-  {
-    // Upload a larger set of source maps for prettier stack traces (increases build time)
-    widenClientFileUpload: true,
-
-    // Transpiles SDK to be compatible with IE11 (increases bundle size)
-    transpileClientSDK: true,
-
-    // Routes browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers (increases server load)
-    tunnelRoute: "/monitoring",
-
-    // Hides source maps from generated client bundles
-    hideSourceMaps: true,
-
-    // Automatically tree-shake Sentry logger statements to reduce bundle size
-    disableLogger: true,
-
-    // Enables automatic instrumentation of Vercel Cron Monitors.
-    // See the following for more information:
-    // https://docs.sentry.io/product/crons/
-    // https://vercel.com/docs/cron-jobs
-    automaticVercelMonitors: true,
-  }
-);
+export default config;
