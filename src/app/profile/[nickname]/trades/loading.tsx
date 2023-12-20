1a5bd8b28921880612341421aa449f21ba057da4
xKairu <kairu@team-reflex.com>
2023-12-20 20:19:54 +1300

    fix trades loading state

--- src/app/profile/[nickname]/trades/loading.tsx
+++ src/app/profile/[nickname]/trades/loading.tsx
@@ -1,58 +1,33 @@
-import { Loader } from "@/components/loader";
 import { PropsWithClassName, cn } from "@/lib/utils";
 
 export default function UserTransfersLoading() {
   return (
-    <div className="container flex flex-col py-2">
-      <div className="flex flex-col sm:gap-2 group" data-show={false}>
+    <div className="container flex flex-col gap-2 py-2">
+      <div className="flex flex-col">
         {/* header */}
         <div className="flex items-center justify-between pb-2 sm:pb-0">
           {/* title */}
           <div className="flex gap-2 items-center">
-            <Skeleton className="w-48 h-10" />
+            <h1 className="text-3xl font-cosmo uppercase drop-shadow-lg">
+              Trades
+            </h1>
           </div>
 
           {/* desktop: options */}
           <div className="hidden sm:flex items-center gap-2">
-            {/* copy address button */}
-            <Skeleton className="w-24 h-10" />
-            {/* polygon button */}
-            <Skeleton className="w-24 h-10" />
-            {/* opensea button */}
-            <Skeleton className="w-24 h-10" />
-            {/* objekt lists button */}
-            <Skeleton className="w-24 h-10" />
+            {/* profile button */}
+            <Skeleton className="w-24 h-9" />
           </div>
 
           {/* mobile: options */}
           <div className="flex sm:hidden items-center gap-2">
-            {/* show filters */}
-            <Skeleton className="w-8 h-8" />
-            {/* options dropdown */}
+            {/* profile */}
             <Skeleton className="w-8 h-8" />
           </div>
         </div>
-
-        {/* filters */}
-        <div className="transition-all flex sm:group-data-[show=false]:visible sm:group-data-[show=true]:visible sm:group-data-[show=false]:opacity-100 sm:group-data-[show=true]:opacity-100 group-data-[show=true]:pb-2 sm:pb-1 sm:group-data-[show=false]:h-12 sm:group-data-[show=true]:h-12 group-data-[show=false]:h-0 group-data-[show=false]:invisible group-data-[show=false]:opacity-0 group-data-[show=true]:h-36 gap-2 items-center flex-wrap justify-center">
-          {Array.from({ length: 7 }).map((_, i) => (
-            <Skeleton key={i} className="w-24 h-10" />
-          ))}
-        </div>
       </div>
 
-      <div className="flex flex-col">
-        <div className="flex flex-row gap-2 pt-1 pb-1 px-1 h-fit sm:justify-center justify-items-start overflow-x-scroll no-scrollbar">
-          {Array.from({ length: 24 }).map((_, i) => (
-            <Skeleton key={i} className="w-10 h-10 rounded-full" />
-          ))}
-        </div>
-        <div className="flex flex-col items-center">
-          <div className="grid grid-cols-3 md:grid-cols-4 gap-4 py-2">
-            <Loader className="col-span-full" />
-          </div>
-        </div>
-      </div>
+      <Skeleton className="w-full h-48" />
     </div>
   );
 }
