834b773dc8943704174f4d8ed9cb41881dcdcc98
xKairu <kairu@team-reflex.com>
2024-01-03 23:11:37 +1300

    remove filters from trades endpoint

--- src/lib/universal/objekts.ts
+++ src/lib/universal/objekts.ts
@@ -1,12 +1,5 @@
 import type { Collection } from "@/lib/server/db/indexer/schema";
-import {
-  ValidArtist,
-  ValidClass,
-  ValidOnlineType,
-  ValidSeason,
-  ValidSort,
-  validSorts,
-} from "./cosmo/common";
+import { validSorts } from "./cosmo/common";
 export type {
   ObjektList,
   CreateObjektList,
@@ -25,37 +18,6 @@ export type ValidObjekt = OwnedObjekt | IndexedObjekt;
 
 // available sort options when using the chain as a data source
 export const chainSorts = [...validSorts, "serialAsc", "serialDesc"] as const;
-
-// TODO: replace with zod parsing
-export type ParsedObjektParams = {
-  list?: string | null;
-  address?: string | null;
-  page: number;
-  sort: ValidSort;
-  season: ValidSeason[];
-  class: ValidClass[];
-  on_offline: ValidOnlineType[];
-  member?: string | null;
-  artist?: ValidArtist;
-  collectionNo: string[];
-};
-
-export function parseParams(params: URLSearchParams): ParsedObjektParams {
-  return {
-    list: params.has("list") ? params.get("list") : undefined,
-    address: params.has("address") ? params.get("address") : undefined,
-    page: parseInt(params.get("page") ?? "1"),
-    sort: params.has("sort") ? (params.get("sort") as ValidSort) : "newest",
-    season: params.getAll("season") as ValidSeason[],
-    class: params.getAll("class") as ValidClass[],
-    on_offline: params.getAll("on_offline") as ValidOnlineType[],
-    member: params.has("member") ? params.get("member") : undefined,
-    artist: params.has("artist")
-      ? (params.get("artist") as ValidArtist)
-      : undefined,
-    collectionNo: params.getAll("collectionNo"),
-  };
-}
 
 export function parsePage<T>(data: any) {
   return {
