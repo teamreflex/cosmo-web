81f5bdfbdf9d4817eba965c1aedbcadeb6ecef64
xKairu <kairu@team-reflex.com>
2024-01-27 22:26:00 +1300

    and gravity index skeleton

--- src/app/gravity/loading.tsx
+++ src/app/gravity/loading.tsx
@@ -1,5 +1,15 @@
-import { Loader } from "@/components/loader";
+import { GravitySkeleton } from "@/components/gravity/gravity-renderer";
 
 export default function GravityLoading() {
-  return <Loader>Loading gravity...</Loader>;
+  return (
+    <main className="container flex flex-col py-2">
+      <div className="flex items-center">
+        <div className="flex gap-2 items-center">
+          <h1 className="text-3xl font-cosmo uppercase">Gravity</h1>
+        </div>
+      </div>
+
+      <GravitySkeleton />
+    </main>
+  );
 }
