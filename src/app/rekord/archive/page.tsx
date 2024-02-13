11002141b221507ad04c7709eae7641f26cd206f
kylekz <kairu@team-reflex.com>
2024-02-13 20:26:16 +1300

    update archive placeholder

--- src/app/rekord/archive/page.tsx
+++ src/app/rekord/archive/page.tsx
@@ -1,18 +1,15 @@
 import { Metadata } from "next";
-import { decodeUser } from "@/app/data-fetching";
 
 export const metadata: Metadata = {
-  title: "Rekord",
+  title: "Rekord Archive",
 };
 
 export default async function RekordArchivePage() {
-  const user = await decodeUser();
-
   return (
     <main className="container flex flex-col py-2">
       <div className="flex items-center">
         <div className="flex gap-2 items-center">
-          <h1 className="text-3xl font-cosmo uppercase">Rekord</h1>
+          <h1 className="text-3xl font-cosmo uppercase">Rekord Archive</h1>
         </div>
       </div>
 
