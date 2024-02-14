0865318e0a9f6f97ba70add9c33e996316b0ef24
kylekz <kairu@team-reflex.com>
2024-02-15 08:38:39 +1300

    remove guest code from homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,45 +1,18 @@
 import { Suspense } from "react";
-import Image from "next/image";
 import NewsRenderer from "@/components/news/news-renderer";
 import { Metadata } from "next";
-import { env } from "@/env.mjs";
-import { decodeUser } from "./data-fetching";
 import ApolloErrorBoundary from "@/components/error-boundary";
-import LogoImage from "@/assets/logo.png";
 
 export const metadata: Metadata = {
   title: "Home",
 };
 
 export default async function HomePage() {
-  const user = await decodeUser();
-
-  if (!user) {
-    return (
-      <span className="flex flex-col justify-center items-center w-full gap-2 py-12">
-        <Image
-          src={LogoImage.src}
-          width={100}
-          height={100}
-          quality={100}
-          alt={env.NEXT_PUBLIC_APP_NAME}
-        />
-        <span className="font-semibold text-lg">
-          Welcome to{" "}
-          <span className="font-cosmo uppercase">
-            {env.NEXT_PUBLIC_APP_NAME}
-          </span>
-          <p className="text-sm text-center">Please sign in</p>
-        </span>
-      </span>
-    );
-  }
-
   return (
     <main className="flex flex-col items-center container py-2">
       <Suspense fallback={<NewsSkeleton />}>
         <ApolloErrorBoundary message="Could not load news">
-          <NewsRenderer user={user} />
+          <NewsRenderer />
         </ApolloErrorBoundary>
       </Suspense>
     </main>
--- src/components/news/news-renderer.tsx
+++ src/components/news/news-renderer.tsx
@@ -4,21 +4,17 @@ import {
   isFeedSection,
   isRekordSection,
 } from "@/lib/universal/cosmo/news";
-import { TokenPayload } from "@/lib/universal/auth";
 import NewsSectionFeed from "./news-section-feed";
 import NewsSectionExclusive from "./news-section-exclusive";
 import NewsSectionBanner from "./news-section-banner";
-import { getProfile } from "@/app/data-fetching";
+import { decodeUser, getProfile } from "@/app/data-fetching";
 import { fetchHomeNews } from "@/lib/server/cosmo/news";
 import NewsSectionRekord from "./news-section-rekord";
 
-type Props = {
-  user: TokenPayload;
-};
-
-export default async function NewsRenderer({ user }: Props) {
-  const profile = await getProfile(user.profileId);
-  const news = await fetchHomeNews(user.accessToken, profile.artist);
+export default async function NewsRenderer() {
+  const user = await decodeUser();
+  const profile = await getProfile(user!.profileId);
+  const news = await fetchHomeNews(user!.accessToken, profile.artist);
 
   return (
     <div className="flex flex-col items-center container px-4">
