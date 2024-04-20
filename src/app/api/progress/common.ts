7699f360d0d8adf6a8562a0a5be8e7bacaee683d
kylekz <kairu@team-reflex.com>
2024-04-20 16:20:59 +1200

    remove caching on progress total fetching

--- src/app/api/progress/common.ts
+++ src/app/api/progress/common.ts
@@ -2,40 +2,35 @@ import { indexer } from "@/lib/server/db/indexer";
 import { collections } from "@/lib/server/db/indexer/schema";
 import { ValidOnlineType } from "@/lib/universal/cosmo/common";
 import { and, eq, inArray, not } from "drizzle-orm";
-import { unstable_cache } from "next/cache";
 
 /**
  * Fetch all collections for the given member.
- * Cached for 15 minutes.
  */
-export const fetchTotal = unstable_cache(
-  async (member: string, onlineType: ValidOnlineType | null = null) => {
-    const result = await indexer
-      .select({
-        id: collections.id,
-        collectionNo: collections.collectionNo,
-        frontImage: collections.frontImage,
-        textColor: collections.textColor,
-        member: collections.member,
-        season: collections.season,
-        class: collections.class,
-        onOffline: collections.onOffline,
-      })
-      .from(collections)
-      .where(
-        and(
-          eq(collections.member, member),
-          not(inArray(collections.class, ["Welcome", "Zero"])),
-          ...(onlineType !== null
-            ? [eq(collections.onOffline, onlineType)]
-            : [])
-        )
-      );
+export async function fetchTotal(
+  member: string,
+  onlineType: ValidOnlineType | null = null
+) {
+  const result = await indexer
+    .select({
+      id: collections.id,
+      collectionNo: collections.collectionNo,
+      frontImage: collections.frontImage,
+      textColor: collections.textColor,
+      member: collections.member,
+      season: collections.season,
+      class: collections.class,
+      onOffline: collections.onOffline,
+    })
+    .from(collections)
+    .where(
+      and(
+        eq(collections.member, member),
+        not(inArray(collections.class, ["Welcome", "Zero"])),
+        ...(onlineType !== null ? [eq(collections.onOffline, onlineType)] : [])
+      )
+    );
 
-    return result;
-  },
-  ["collections-for-member"], // param (member name) gets added to this
-  { revalidate: 60 * 15 } // 15 minutes
-);
+  return result;
+}
 
 export type PartialCollection = Awaited<ReturnType<typeof fetchTotal>>;
