de3e5c63b054fc5752e2243672e0f828b77012ca
kylekz <kairu@team-reflex.com>
2024-05-16 02:14:07 +1200

    force client & inline

--- src/components/misc/hls-video.tsx
+++ src/components/misc/hls-video.tsx
@@ -1,5 +1,8 @@
+"use client";
+
 import "@vidstack/react/player/styles/base.css";
 import { MediaPlayer, MediaProvider, Poster } from "@vidstack/react";
+import Hydrated from "../hydrated";
 
 type Props = {
   videoUrl: string;
@@ -9,22 +12,25 @@ type Props = {
 
 export default function HLSVideo({ videoUrl, thumbnailUrl, title }: Props) {
   return (
-    <MediaPlayer
-      title={title}
-      src={videoUrl}
-      controls={true}
-      viewType="video"
-      streamType="on-demand"
-    >
-      <MediaProvider>
-        {thumbnailUrl !== undefined && (
-          <Poster
-            className="absolute inset-0 block h-full w-full rounded-xl opacity-0 transition-opacity data-[visible]:opacity-100 [&>img]:h-full [&>img]:w-full [&>img]:object-cover"
-            src={thumbnailUrl}
-            alt={title ?? "video"}
-          />
-        )}
-      </MediaProvider>
-    </MediaPlayer>
+    <Hydrated>
+      <MediaPlayer
+        title={title}
+        src={videoUrl}
+        controls={true}
+        viewType="video"
+        streamType="on-demand"
+        playsInline={true}
+      >
+        <MediaProvider>
+          {thumbnailUrl !== undefined && (
+            <Poster
+              className="absolute inset-0 block h-full w-full rounded-xl opacity-0 transition-opacity data-[visible]:opacity-100 [&>img]:h-full [&>img]:w-full [&>img]:object-cover"
+              src={thumbnailUrl}
+              alt={title ?? "video"}
+            />
+          )}
+        </MediaProvider>
+      </MediaPlayer>
+    </Hydrated>
   );
 }
