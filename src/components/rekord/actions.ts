7b84150bf5fca79d7b757474edbfdfa8aba91548
kylekz <kairu@team-reflex.com>
2024-05-27 04:30:57 +1200

    track rekord like/unlike

--- src/components/rekord/actions.ts
+++ src/components/rekord/actions.ts
@@ -7,10 +7,9 @@ import { z } from "zod";
 /**
  * Toggle the like status of the given rekord post.
  */
-export const toggleLike = async (prevState: unknown, form: FormData) =>
+export const toggleLike = async (form: FormData) =>
   authenticatedAction({
     form,
-
     schema: z.object({
       postId: z.coerce.number().positive(),
       isLiked: z.coerce.boolean(),
--- src/components/rekord/rekord-like.tsx
+++ src/components/rekord/rekord-like.tsx
@@ -1,10 +1,10 @@
 import { CosmoRekordPost } from "@/lib/universal/cosmo/rekord";
 import { toggleLike } from "./actions";
-import { useFormState, useFormStatus } from "react-dom";
 import { Button } from "../ui/button";
 import { Heart, Loader2 } from "lucide-react";
 import { cn } from "@/lib/utils";
-import { Dispatch, SetStateAction, useEffect, useState } from "react";
+import { Dispatch, SetStateAction, useTransition } from "react";
+import { trackEvent } from "fathom-client";
 
 type Props = {
   post: CosmoRekordPost;
@@ -13,43 +13,40 @@ type Props = {
 };
 
 export default function RekordLikeButton({ post, isLiked, setIsLiked }: Props) {
-  const [state, formAction] = useFormState(toggleLike, { status: "idle" });
+  const [isPending, startTransition] = useTransition();
 
-  useEffect(() => {
-    if (state.status === "success") {
-      setIsLiked((prev) => !prev);
-    }
-  }, [state, setIsLiked]);
+  function submit(form: FormData) {
+    startTransition(async () => {
+      const result = await toggleLike(form);
+      if (result.status === "success" && result.data) {
+        trackEvent(`${isLiked ? "unlike" : "like"}-rekord`);
+        setIsLiked((prev) => !prev);
+      }
+    });
+  }
 
   return (
     <form
-      action={formAction}
+      action={submit}
       className="absolute z-30 top-1 right-0 flex flex-col items-center drop-shadow"
     >
       <input type="text" name="postId" value={post.id} hidden readOnly />
       <input type="checkbox" name="isLiked" checked={isLiked} hidden readOnly />
-      <SubmitButton isLiked={isLiked} />
+
+      <Button type="submit" disabled={isPending} variant="icon">
+        {isPending ? (
+          <Loader2 className="w-8 h-8 animate-spin" />
+        ) : (
+          <Heart
+            className={cn(
+              "w-8 h-8 drop-shadow",
+              isLiked ? "fill-red-500 text-red-500" : "text-foreground"
+            )}
+          />
+        )}
+      </Button>
 
       <span className="text-sm drop-shadow">{post.totalLikeCount}</span>
     </form>
-  );
-}
-
-function SubmitButton({ isLiked }: { isLiked: boolean }) {
-  const { pending } = useFormStatus();
-
-  return (
-    <Button type="submit" disabled={pending} variant="icon">
-      {pending ? (
-        <Loader2 className="w-8 h-8 animate-spin" />
-      ) : (
-        <Heart
-          className={cn(
-            "w-8 h-8 drop-shadow",
-            isLiked ? "fill-red-500 text-red-500" : "text-foreground"
-          )}
-        />
-      )}
-    </Button>
   );
 }
