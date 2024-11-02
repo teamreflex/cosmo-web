e56f5b59c893a932f9fa0a4256bb94e0c8e6e6ae
kylekz <kairu@team-reflex.com>
2024-11-02 14:11:41 +1300

    add oma code

--- src/lib/server/cosmo/albums.ts
+++ src/lib/server/cosmo/albums.ts
@@ -1,7 +1,7 @@
 import "server-only";
 import { ValidArtist } from "@/lib/universal/cosmo/common";
 import { cosmo } from "../http";
-import { CosmoAlbum } from "@/lib/universal/cosmo/albums";
+import { CosmoAlbum, CosmoAlbumWithTracks } from "@/lib/universal/cosmo/albums";
 
 /**
  * Fetch a list of albums.
@@ -18,16 +18,61 @@ export async function fetchAlbums(token: string, artistName: ValidArtist) {
 }
 
 /**
+ * Fetch a single album.
+ */
+export async function fetchAlbum(token: string, albumHid: string) {
+  return await cosmo<CosmoAlbumWithTracks>(`/bff/v3/albums/${albumHid}`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+}
+
+/**
+ * Get an album by its QR code.
+ */
+export async function fetchAlbumByQrCode(token: string, code: string) {
+  return await cosmo<CosmoAlbumWithTracks>(
+    `/bff/v3/objekt-music-album-qr-codes/album`,
+    {
+      query: {
+        n: code,
+      },
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    }
+  );
+}
+
+/**
  * Claim an album via its QR code.
  */
 export async function claimAlbum(token: string, code: string) {
-  return await cosmo<CosmoAlbum[]>(
-    `/bff/v3/objekt-music-album-qr-codes/album`,
+  return await cosmo<CosmoAlbumWithTracks>(
+    `/bff/v3/objekt-music-album-qr-codes/claim`,
     {
       method: "POST",
       query: {
         n: code,
       },
+      body: {
+        "Accept-Language": "en",
+      },
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    }
+  );
+}
+
+/**
+ * Get an album's download URLs.
+ */
+export async function fetchAlbumDownload(token: string, albumHid: string) {
+  return await cosmo<CosmoAlbumWithTracks>(
+    `/bff/v3/albums/${albumHid}/track/download-authorize`,
+    {
       headers: {
         Authorization: `Bearer ${token}`,
       },
--- src/lib/universal/cosmo/albums.ts
+++ src/lib/universal/cosmo/albums.ts
@@ -1,15 +1,83 @@
 import { CosmoArtist } from "./artists";
 
 export type CosmoAlbum = {
-  hid: string;
-  releasedAt: string;
   artist: CosmoArtist;
   title: string;
   albumImageThumbnailUrl: string;
   omaImageThumbnailUrl: string;
+  hid: string;
+  releasedAt: string;
 };
 
 export type CosmoAlbumClaimError = {
   statusCode: number;
   message: string;
 };
+
+export interface CosmoAlbumWithTracks extends CosmoAlbum {
+  artist: CosmoArtist;
+  title: string;
+  albumImageThumbnailUrl: string;
+  omaImageThumbnailUrl: string;
+  hid: string;
+  releasedAt: string;
+  albumTracks: CosmoAlbumTrack[];
+}
+
+export type CosmoAlbumTrack = {
+  title: string;
+  hid: string;
+  albumHid: string;
+  trackNo: number;
+  duration: number;
+};
+
+export interface CosmoAlbumTrackDownload extends CosmoAlbumTrack {
+  fileAccessUrl: string;
+}
+
+const patterns = [
+  // objekt QR code
+  {
+    type: "objekt",
+    pattern: /[?&]n=([^&]+)/,
+    extract: (match: RegExpMatchArray) => match[1],
+  },
+  // OMA QR code
+  {
+    type: "oma",
+    pattern: /^[a-f0-9]{32}$/,
+    extract: (match: RegExpMatchArray) => match[0],
+  },
+] as const;
+
+type ObjektCode = {
+  type: "objekt";
+  value: string;
+};
+
+type OMACode = {
+  type: "oma";
+  value: string;
+};
+
+type MatchedCode = ObjektCode | OMACode;
+
+export function extractObjektCode(input: string): MatchedCode {
+  for (const { type, pattern, extract } of patterns) {
+    const match = input.match(pattern);
+    if (match) {
+      return {
+        type,
+        value: extract(match),
+      };
+    }
+  }
+  throw new InvalidQRCodeError();
+}
+
+export class InvalidQRCodeError extends Error {
+  constructor() {
+    super("Invalid QR code");
+  }
+}
