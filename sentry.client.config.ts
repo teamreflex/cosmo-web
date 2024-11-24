2d0bea054aec000461b91dda894455a53171be27
kylekz <kairu@team-reflex.com>
2024-11-24 19:01:04 +1300

    check stacktrace before ignoring client errors

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -1,14 +1,26 @@
 import { env } from "@/env.mjs";
 import * as Sentry from "@sentry/nextjs";
 
+const stackPatterns = [
+  // Maximize-Video seems to be the main culprit
+  new RegExp("app:///userscripts"),
+  // can't get around release caching
+  /ChunkLoadError/,
+];
+
 Sentry.init({
   dsn: env.NEXT_PUBLIC_SENTRY_DSN,
   debug: false,
   tracesSampleRate: 0.5,
-  ignoreErrors: [
-    // Maximize-Video seems to be the main culprit
-    "app:///userscripts",
-    // can't get around release caching
-    "ChunkLoadError",
-  ],
+  // ignore any errors with stacktraces that match specific patterns
+  beforeSend(event, hint) {
+    const stack = hint.syntheticException?.stack;
+    if (stack) {
+      if (stackPatterns.some((pattern) => pattern.test(stack))) {
+        return null;
+      }
+    }
+
+    return event;
+  },
 });
