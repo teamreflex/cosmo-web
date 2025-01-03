618b7dde8db3781b5cd9a5ced048ea3730ee90cf
kylekz <kairu@team-reflex.com>
2025-01-03 17:21:16 +1300

    fix overlay

--- src/components/objekt/overlay/action-overlay.tsx
+++ src/components/objekt/overlay/action-overlay.tsx
@@ -47,7 +47,7 @@ export default function ActionOverlay({
     !token.transferable ||
     usedForGrid ||
     isLocked ||
-    isPinned ||
+    (isPinned && isPin) ||
     (token.transferable && authenticated);
 
   return (
