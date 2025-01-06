933394bdd5bad6097ac74521d95c53a574e3851b
kylekz <kairu@team-reflex.com>
2025-01-07 05:34:06 +1300

    migration script cleanup

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -1,4 +1,4 @@
-import { neon, neonConfig } from "@neondatabase/serverless";
+import { neonConfig } from "@neondatabase/serverless";
 import { drizzle } from "drizzle-orm/neon-http";
 import { env } from "@/env.mjs";
 import * as schema from "./schema";
@@ -13,5 +13,4 @@ neonConfig.fetchEndpoint = (host) => {
 };
 
 // create the connection
-const sql = neon(env.DATABASE_URL);
-export const db = drizzle(sql, { schema });
+export const db = drizzle(env.DATABASE_URL, { schema });
