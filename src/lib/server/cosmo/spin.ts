719d0b2719c54e21350c2443e89ee28903b6b05d
kylekz <kairu@team-reflex.com>
2025-03-03 03:44:43 +1300

    misc

--- src/lib/server/cosmo/spin.ts
+++ src/lib/server/cosmo/spin.ts
@@ -1,13 +1,6 @@
 import { ValidArtist } from "@/lib/universal/cosmo/common";
 import { cosmo } from "../http";
-import {
-  CosmoSpinCompleteRequest,
-  CosmoSpinOption,
-  CosmoSpinGetTickets,
-  CosmoSpinPresignResponse,
-  CosmoSpinSeasonRate,
-  CosmoSpinStatisticsResponse,
-} from "@/lib/universal/cosmo/spin";
+import { CosmoSpinGetTickets } from "@/lib/universal/cosmo/spin";
 
 /**
  * steps:
@@ -25,76 +18,5 @@ export async function fetchSpinTickets(token: string, artist: ValidArtist) {
     headers: {
       Authorization: `Bearer ${token}`,
     },
-  });
-}
-
-/**
- * Fetch the statistics for the artist.
- */
-export async function fetchSpinStatistics(token: string, artist: ValidArtist) {
-  return await cosmo<CosmoSpinStatisticsResponse>(
-    `/bff/v3/spin/statistic/${artist}`,
-    {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    }
-  );
-}
-
-/**
- * Fetch the pull rates for the given artist.
- */
-export async function fetchSpinRates(token: string, artist: ValidArtist) {
-  return await cosmo<CosmoSpinSeasonRate[]>(`/bff/v3/spin/rate/${artist}`, {
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-  });
-}
-
-/**
- * Pre-sign a spin with the given token ID.
- */
-export async function presignSpin(token: string, tokenId: number) {
-  return await cosmo<CosmoSpinPresignResponse>(`/bff/v3/spin/pre-sign`, {
-    method: "POST",
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-    body: {
-      usedTokenId: tokenId,
-    },
-  });
-}
-
-/**
- * Submit a spin with the given spin ID.
- */
-export async function submitSpin(token: string, spinId: number) {
-  return await cosmo<void>(`/bff/v3/spin`, {
-    method: "POST",
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-    body: {
-      spinId,
-    },
-  }).then(() => true);
-}
-
-/**
- * Submit the chosen objekt for the spin.
- */
-export async function completeSpin(
-  token: string,
-  params: CosmoSpinCompleteRequest
-) {
-  return await cosmo<CosmoSpinOption[]>(`/bff/v3/spin/complete`, {
-    method: "POST",
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-    body: params,
   });
 }
