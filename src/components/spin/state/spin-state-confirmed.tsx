d9fafbea862fba2a1b8b58bf7e2cbcd5f0730430
kylekz <kairu@team-reflex.com>
2025-03-04 19:02:17 +1300

    fix selecting index 0

--- src/components/spin/state/spin-state-confirmed.tsx
+++ src/components/spin/state/spin-state-confirmed.tsx
@@ -27,7 +27,8 @@ export default function StateConfirmed({ state }: Props) {
   }
 
   function handleConfirm() {
-    if (!index) return;
+    if (index === undefined) return;
+
     mutate(
       {
         spinId: state.spinId,
