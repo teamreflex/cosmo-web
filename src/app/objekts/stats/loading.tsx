1e2654d99fe97cb4c00cd412d40b1e79c0a94b14
kylekz <kairu@team-reflex.com>
2025-04-10 21:16:59 +1200

    fix objekt stats suspense fallback

--- src/app/objekts/stats/loading.tsx
+++ src/app/objekts/stats/loading.tsx
@@ -16,8 +16,8 @@ export default async function ObjektStatsLoading() {
       <div className="flex flex-col gap-4">
         {/* totals */}
         <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-3">
-          <Card className="h-36">
-            <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+          <Card className="min-h-36">
+            <CardHeader className="flex flex-row items-center justify-between space-y-0">
               <CardTitle className="text-sm font-semibold">
                 Total Objekts
               </CardTitle>
@@ -30,8 +30,8 @@ export default async function ObjektStatsLoading() {
             </CardContent>
           </Card>
 
-          <Card className="h-36">
-            <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+          <Card className="min-h-36">
+            <CardHeader className="flex flex-row items-center justify-between space-y-0">
               <CardTitle className="text-sm font-semibold">
                 Scanned Objekts
               </CardTitle>
@@ -44,8 +44,8 @@ export default async function ObjektStatsLoading() {
             </CardContent>
           </Card>
 
-          <Card className="h-36">
-            <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+          <Card className="min-h-36">
+            <CardHeader className="flex flex-row items-center justify-between space-y-0">
               <CardTitle className="text-sm font-semibold">
                 Premier Objekts
               </CardTitle>
