995baaca97f07e9411b6eb1975ad710e8e0235b4
kylekz <kairu@team-reflex.com>
2025-04-20 22:52:49 +1200

    deps

--- src/lib/server/cookies.ts
+++ src/lib/server/cookies.ts
@@ -1,6 +1,4 @@
 import { cookies } from "next/headers";
-import { generateCookiePayload } from "@/lib/server/jwt";
-import { ResponseCookie } from "next/dist/compiled/@edge-runtime/cookies";
 
 type CookieKey = "artist" | "token" | "user-session";
 
@@ -10,29 +8,4 @@ type CookieKey = "artist" | "token" | "user-session";
 export async function getCookie<T = string>(key: CookieKey) {
   const store = await cookies();
   return store.get(key)?.value as T | undefined;
-}
-
-type SetCookie = {
-  key: CookieKey;
-  value: string;
-  cookie?: Partial<ResponseCookie>;
-};
-
-/**
- * Save a new cookie.
- */
-export async function setCookie({ key, value, cookie }: SetCookie) {
-  const store = await cookies();
-  return store.set(key, value, {
-    ...generateCookiePayload(),
-    ...cookie,
-  });
-}
-
-/**
- * Delete a cookie.
- */
-export async function deleteCookie(key: CookieKey) {
-  const store = await cookies();
-  return store.delete(key);
 }
