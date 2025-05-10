60f97876a125306026f87d6c7d23b16c2efc4375
kylekz <kairu@team-reflex.com>
2025-05-10 22:43:47 +1200

    check auth in admin page

--- src/app/admin/metadata/page.tsx
+++ src/app/admin/metadata/page.tsx
@@ -1,14 +1,21 @@
+import { getSession } from "@/app/data-fetching";
 import InsertMetadata from "@/components/admin/metadata/insert-metadata";
 import LatestMetadata from "@/components/admin/metadata/latest-metadata";
 import Skeleton from "@/components/skeleton/skeleton";
 import { Metadata } from "next";
 import { Suspense } from "react";
+import { redirect } from "next/navigation";
 
 export const metadata: Metadata = {
   title: "Objekt Metadata",
 };
 
 export default async function AdminObjektMetadataPage() {
+  const session = await getSession();
+  if (!session?.user.isAdmin) {
+    redirect("/");
+  }
+
   return (
     <section className="flex flex-col gap-2 px-4 py-2">
       <InsertMetadata />
