2adcaba9caf4c9080f58745146ab1aa6d61f899b
kylekz <kairu@team-reflex.com>
2025-05-13 04:36:18 +1200

    gravity data clean up

---
+++ drizzle/0027_messy_silver_sable.sql
@@ -0,0 +1,4 @@
+-- old gravity data has some broken images, just refetch after this
+DELETE FROM "gravities";--> statement-breakpoint
+DELETE FROM "gravity_polls";--> statement-breakpoint
+DELETE FROM "gravity_poll_candidates";
