5371ee4b27750f2e15bbe897845a80b0920f48c0
kylekz <kairu@team-reflex.com>
2025-11-08 08:47:16 +1300

    optimize serial hook

--- apps/web/src/hooks/use-objekt-serial.tsx
+++ apps/web/src/hooks/use-objekt-serial.tsx
@@ -1,38 +1,35 @@
 import { useNavigate, useSearch } from "@tanstack/react-router";
-import { useCallback } from "react";
 
 /**
  * Manages the objekt serial from within the metadata dialog.
  */
 export function useObjektSerial() {
   const navigate = useNavigate();
-  const search = useSearch({
+  const searchSerial = useSearch({
     strict: false,
+    select: (search) => search.serial ?? undefined,
   });
 
-  const setSerial = useCallback(
-    (
-      serial:
-        | number
-        | undefined
-        | ((prev: number | undefined) => number | undefined),
-    ) => {
-      if (typeof serial === "function") {
-        serial = serial(search.serial ?? undefined);
-      }
+  const setSerial = (
+    serial:
+      | number
+      | undefined
+      | ((prev: number | undefined) => number | undefined),
+  ) => {
+    if (typeof serial === "function") {
+      serial = serial(searchSerial);
+    }
 
-      navigate({
-        // @ts-ignore - this hook is used on different routes so we can't reliably type this
-        search: (prev) => ({ ...prev, serial }),
-        replace: true,
-      });
-    },
-    [navigate, search],
-  );
+    navigate({
+      // @ts-ignore - this hook is used on different routes so we can't reliably type this
+      search: (prev) => ({ ...prev, serial }),
+      replace: true,
+    });
+  };
 
-  const reset = useCallback(() => {
+  function reset() {
     setSerial(undefined);
-  }, [setSerial]);
+  }
 
-  return { serial: search.serial ?? undefined, setSerial, reset };
+  return { serial: searchSerial, setSerial, reset };
 }
