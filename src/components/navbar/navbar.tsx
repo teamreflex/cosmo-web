06f3efe33d35f712eed82a6c5770e4e964fa4947
kylekz <kairu@team-reflex.com>
2025-11-12 21:09:04 +1300

    add mco notice

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -16,7 +16,15 @@ import AuthFallback from "../auth/auth-fallback";
 import { ArtistProvider } from "@/hooks/use-artists";
 import { Skeleton } from "../ui/skeleton";
 import { IconCards } from "@tabler/icons-react";
-import { ChartColumnBig, Menu, Search, Vote } from "lucide-react";
+import {
+  AlertTriangle,
+  ChartColumnBig,
+  Menu,
+  Search,
+  Vote,
+} from "lucide-react";
+import { Popover, PopoverContent, PopoverTrigger } from "../ui/popover";
+import { Separator } from "../ui/separator";
 
 export default function Navbar() {
   return (
@@ -30,6 +38,39 @@ export default function Navbar() {
                 <SystemStatus />
                 <UpdateDialog />
               </div>
+              <Popover>
+                <PopoverTrigger asChild>
+                  <button className="relative h-8 w-9 flex justify-center items-center rounded-md bg-orange-500/40 hover:bg-orange-500/55 transition-colors">
+                    <AlertTriangle className="text-orange-500 w-5 h-5" />
+                  </button>
+                </PopoverTrigger>
+                <PopoverContent className="flex flex-col text-xs gap-4">
+                  <p>
+                    <b>tldr:</b> It will take some time to properly support
+                    MCOs, but we&apos;re working on it.
+                  </p>
+
+                  <Separator orientation="horizontal" />
+
+                  <div className="flex flex-col gap-1">
+                    <p>
+                      Similar to how the borders for idntt objekts are separate
+                      from the actual image, the video files for MCOs are also
+                      separate.
+                    </p>
+                    <p>
+                      The files for these are not publicly accessible like the
+                      front and back images are. They can only be accessed by
+                      owning the specific objekt and reading what the app does.
+                    </p>
+                    <p>
+                      This means that we cannot automate the process of loading
+                      the videos into Apollo. These files will need to be
+                      manually gathered from owners and added to the database.
+                    </p>
+                  </div>
+                </PopoverContent>
+              </Popover>
             </div>
 
             <ErrorBoundary fallback={<AuthFallback />}>
