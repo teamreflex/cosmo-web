befec9d86dd50926d3b92142e777e5f3ca4adce3
kylekz <kairu@team-reflex.com>
2026-01-01 03:51:38 +1300

    fix carousel centering

--- apps/web/src/components/events/active-events-carousel.tsx
+++ apps/web/src/components/events/active-events-carousel.tsx
@@ -63,6 +63,7 @@ export default function ActiveEventsCarousel({
         opts={{
           loop: true,
           align: "center",
+          containScroll: false,
         }}
         plugins={[autoplay.current]}
         setApi={setApi}
