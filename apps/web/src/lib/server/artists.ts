3615146aaf969ef9ad863f20ed127c8c067c8d35
kylekz <kairu@team-reflex.com>
2026-01-05 13:30:32 +1300

    update artists cache

--- apps/web/src/lib/server/artists.ts
+++ apps/web/src/lib/server/artists.ts
@@ -1,26 +1,16 @@
 import { fetchArtist, fetchArtists } from "@apollo/cosmo/server/artists";
 import type { CosmoArtistWithMembersBFF } from "@apollo/cosmo/types/artists";
 import { createServerFn } from "@tanstack/react-start";
-import { setResponseHeaders } from "@tanstack/react-start/server";
-import { cacheHeaders, remember } from "./cache";
+import { remember } from "./cache";
 import { getProxiedToken } from "./proxied-token";
-
-const cacheKey = "artists";
 
 /**
  * Fetch artists and full member data from COSMO.
- * Cached for:
- * - browser: 30 seconds
- * - CDN: 1 hour
- * - Redis: 24 hours
+ * Cached for 1 hour.
  */
 export const $fetchArtists = createServerFn({ method: "GET" }).handler(
   async () => {
-    setResponseHeaders(
-      new Headers(cacheHeaders({ cdn: 60 * 60, tags: [cacheKey] })),
-    );
-
-    return await remember(cacheKey, 60 * 60 * 24, async () => {
+    return await remember("artists", 60 * 60, async () => {
       const { accessToken } = await getProxiedToken();
       const artists = await fetchArtists(accessToken);
       const withMembers = await Promise.all(
