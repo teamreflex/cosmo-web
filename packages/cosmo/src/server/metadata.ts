ea469d3d81c8b92a7b7602696b79c727fdfaf080
kylekz <kairu@team-reflex.com>
2026-01-23 09:38:36 +1300

    fix metadata

--- packages/cosmo/src/server/metadata.ts
+++ packages/cosmo/src/server/metadata.ts
@@ -1,17 +1,20 @@
+import { ofetch } from "ofetch";
 import type {
   CosmoObjektMetadataV1,
   CosmoObjektMetadataV3,
 } from "../types/metadata";
-import { cosmo } from "./http";
 
 /**
  * Fetch objekt metadata from the v1 API.
  */
 export async function fetchMetadataV1(tokenId: string) {
-  return await cosmo<CosmoObjektMetadataV1>(`/objekt/v1/token/${tokenId}`, {
-    retry: 4,
-    retryDelay: 750, // 750ms backoff
-  });
+  return await ofetch<CosmoObjektMetadataV1>(
+    `https://api.cosmo.fans/objekt/v1/token/${tokenId}`,
+    {
+      retry: 4,
+      retryDelay: 750, // 750ms backoff
+    },
+  );
 }
 
 /**
@@ -19,11 +22,8 @@ export async function fetchMetadataV1(tokenId: string) {
  * Shouldn't be used as it doesn't contain full collection data.
  */
 export async function fetchMetadataV3(tokenId: string) {
-  return await cosmo<CosmoObjektMetadataV3>(
-    `/bff/v3/objekts/nft-metadata/${tokenId}`,
-    {
-      retry: 2,
-      retryDelay: 500, // 500ms backoff
-    },
+  return await ofetch<CosmoObjektMetadataV3>(
+    `https://api.cosmo.fans/bff/v3/objekts/nft-metadata/${tokenId}`,
+    { retry: 2, retryDelay: 500 }, // 500ms backoff
   );
 }
